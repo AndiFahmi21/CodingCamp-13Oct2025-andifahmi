e49d41981118670128ff2386de42d10d83b44b63
AndiFahmi21 <fahmiandi470@gmail.com>
2025-10-17 00:16:38 +0800

    konfigurasi kedua kali tool todo list

--- js/script.js
+++ js/script.js
@@ -1,19 +1,53 @@
+// Database Simulation
+let taskDB = [];
+
+// Add Functionality
 function addTask() {
-     const taskInput =
+     // Create Task Object
+     const taskInput = document.getElementbyId('todo-input');
+     const taskDate = document.getElementById('todo-date');
+     
+     if (ValidateInput(taskInput.value, taskDate.value)) {
+          const newTask = {
+               task: taskInput.value,
+               date: taskDate.value,
+          }
+
+          // Add to database
+          taskDb.push(newTask);
+
+          // Render
+          renderTasks();
+     }
 }
 
+// Render Functionality
 function renderTasks() {
-     const tasklist = document.getElemenetby
+     // Clear Existing List
+     const tasklist = document.getElementbyId('task-list');
+     taskList.innerHTML = '';
+
+     // Check if taskDb is empty
+     taskDb.foreach((taskObj, index) => {
+          taskList.innerHTML += `<li>${taskObj.date} <button onclick="deleteTask(${index})">Delete</button></li>`
+     });
 }
 
+// Delete All Functionality
 function deleteAllTasks() {
+     // Clear Database
      taskDb = [];
+
+     // Render
      renderTasks();
 }
 
+// Filter Functionality (placeholder)
 function filterTasks() {}
 
+// Input Validation
 function ValidateInput(task, date) {
+     // Simple Validation
      if (task.trim() === '' || date.trim() === '') {
           alert('Please enter both task and due date.');
           return false;
